f32ada962744ee1a175851620a86772bd94a297f
Damien Luzzo <damienluzzo33@gmail.com>
2021-09-26 02:14:06 -0500

    add basic styles, create js fuctionality for start button, timer countdown, and prompting of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,11 +89,96 @@ var quiz = document.getElementById("quiz");
 var start = document.getElementById("start-button");
 
 // Functions Go Here
+function startTimer() {
+    // Define countdown starting point
+    var timeRemaining = 120;
+    // Create paragraph tag to contain count down timer
+    var clock = document.createElement("p");
+    // set clock's text content to show the time at the start of the counter so that question and time render at the same time
+    clock.textContent = "2:00";
+    // Append clock element p tag as first child of quiz section
+    quiz.appendChild(clock);
+    // decrement the time remaining here to sync with the slight delay of the setInterval function
+    timeRemaining --;
+    // create set interval
+    var countDown = setInterval(function() {
+        // while time remaining is greater than 60 seconds
+        if (timeRemaining > 69) {
+            // set clock's text content to show the time remaining
+            clock.textContent = "1:" + (timeRemaining - 60);
+            // decrement the time remaining during every interval where this if condition is met
+            timeRemaining --;
+            // while there are more than 10 seconds on the clock
+        } // while time remaining is greater than 60 seconds
+        else if (timeRemaining > 59) {
+            // set clock's text content to show the time remaining
+            clock.textContent = "1:0" + (timeRemaining - 60);
+            // decrement the time remaining during every interval where this if condition is met
+            timeRemaining --;
+        
+        } else if (timeRemaining > 10) {
+            // set clock's text content to show the time remaining
+            clock.textContent = "0:" + timeRemaining;
+            // decrement the time remaining during every interval where this if condition is met
+            timeRemaining --;
+            // if there are 10 seconds on the clock, text will turn red
+        } else if (timeRemaining > 9) {
+            // set color of the clock text to be red to indicate that time is running out
+            clock.setAttribute("style", "color: #880808");
+            // set clock's text content to show the time remaining
+            clock.textContent = "0:10";
+            // decrement the time remaining during every interval where this if condition is met
+            timeRemaining --;
+            // if there are 9 seconds on the clock, text will stay red
+        } else if (timeRemaining >= 0) {
+            // keep color of the clock text red and start transition to cadmium red background color to initiate more urgency
+            clock.setAttribute("style", "color: #880808");
+            quiz.setAttribute("style", "background-color: #ee4c2ba4")
+            // set clock's text content to show the time remaining
+            clock.textContent = "0:0" + timeRemaining;
+            // decrement the time remaining during every interval where this if condition is met
+            timeRemaining --;
+        } else {
+            // when timer reaches 0, clock text content is set to an empty string
+            clock.textContent = "";
+            // and countdown interval function is escaped using clearInterval
+            clearInterval(countDown);
+            // get rid of the timer
+            quiz.removeChild(clock);
+        }
+        // each interval is 1 second in duration
+    }, 1000);
+}
 
+var questionIndex = 0;
+var numQuestions = 8;
+
+// This is a function to present questions to user with radio button options to choose from accompanied by a submit button
+function promptQuestions() {
+    // create a new form element to store question, options, and button
+    var form = document.createElement("form");
+    // add form to the quiz section
+    quiz.appendChild(form);
+    // add question text p tag
+    var questionText = document.createElement("p");
+    // add question text element to the form
+    form.appendChild(questionText);
+    // update question text to reflect the text of the current question
+    questionText.textContent = triviaFacts[questionIndex].question;
+    // for each question, create a label and an input for each choice
+    for (var i = 0; i < numQuestions; i++) {
+        // TODO: PUT LOGIC HERE
+    }
+}
 
 // This is a function to initiate the beginning of the quiz
 function startQuiz() {
     console.log("START");
+    // hide the start quiz button
+    start.setAttribute("style", "display: none");
+    // call the startTimer function to start the countdown for the user
+    startTimer();
+    promptQuestions();
 }
 
 // Event listeners for the start quiz button, which when clicked will call the startQuiz function to kick off the sequence of functions that run the application
@@ -102,4 +187,25 @@ start.addEventListener("click", startQuiz);
 
 // PSEUDO CODE HERE : 
 
-//
+// Webpage needs to be responsive
+
+// Provide a set of instructions above the start-quiz button that tells the user (1) there will be 8 multiple choice questions and (2) the user will have 120 seconds to read and choose an answer for each question
+
+// if the time permits, add a hint when timer reaches 15 seconds left on the clock
+
+// When the start-quiz button is clicked by the user, the instructions and start-quiz button will disappear
+
+// After instructions and button are removed from view, the user will be prompted with the first question from the array of "trivia" array
+
+// User will be prompted with four radio buttons when submitting answers to quiz questions and will submit answer by clicking submit button
+
+// if the user chooses incorrectly, 10 seconds will be deducted from the timer
+
+// When the user submits an answer, the user will be prompted with another question
+
+// if the user submits responses for all questions, or if the timer runs down to zero, the quiz is over
+
+// when the quiz is over, the user will be presented with (1) their overall score, (2) an associated grade, (3) correct answers for the questions they answered incorrectly (and if time permits, explanations for the correct response), and the option to add their initials and save their score
+
+// user will then be prompted to start over if they choose
+
